3baf58b3be8340be17b53b53bb4944148c979265
DanielForsell <daniel.forsell@student.oulu.fi>
2024-06-19 20:49:12 +0300

    update 13

--- palauitusrepositorio-main/osa 3/puhelinLuetteloFrontEnd3.9-3.11/vite.config.js
+++ palauitusrepositorio-main/osa 3/puhelinLuetteloFrontEnd3.9-3.11/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://my-backend.onrender.com',
+        target: 'http://localhost:3001',
         changeOrigin: true,
       },
     }
